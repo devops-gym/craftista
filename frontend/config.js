1362e15fa0de576a80917fdc470ee19fb30f2dfa
kiranbaddi <kiran.baddi@hotmail.com>
2025-06-26 22:41:12 +0200

    BREAKING: First working version

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+module.exports = 
+{
+  "version": "1.0.0",	
+  "productsApiBaseUri": process.env.CATALOGUE_URI,
+  "recommendationBaseUri": process.env.RECOMMENDATIONS_URI,
+  "votingBaseUri": process.env.VOTING_URI
+}
---
+++ recommendation/README.md
@@ -0,0 +1,6 @@
+How to Build Recommendation App
+
+- Build Tool : go (Tested with version 1.20)
+- Build Command : go build -o app
+- Port: 9000
+- Launch Command: ./app
